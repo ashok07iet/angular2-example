448274768ee963476b2dadb369accdf92b22314e
Chourasia, Ashok <ashok.chourasia@vce.com>
2017-07-01 19:26:18 +0530

    commit

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -5,11 +5,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var core_1 = require("@angular/core");
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var platform_browser_1 = require("@angular/platform-browser");
+var core_1 = require("@angular/core");
 var app_component_1 = require("./app.component");
-var missioncontrol_component_1 = require("./missioncontrol.component");
-var astronaut_component_1 = require("./astronaut.component");
+var hero_job_ad_component_1 = require("./hero-job-ad.component");
+var ad_banner_component_1 = require("./ad-banner.component");
+var hero_profile_component_1 = require("./hero-profile.component");
+var ad_directive_1 = require("./ad.directive");
+var ad_service_1 = require("./ad.service");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -17,17 +23,17 @@ var AppModule = (function () {
 }());
 AppModule = __decorate([
     core_1.NgModule({
-        imports: [
-            platform_browser_1.BrowserModule
-        ],
-        declarations: [
-            app_component_1.AppComponent,
-            missioncontrol_component_1.MissionControllerComponent,
-            astronaut_component_1.AstronautComponent
-        ],
-        bootstrap: [app_component_1.AppComponent],
-        providers: []
-    })
+        imports: [platform_browser_1.BrowserModule],
+        providers: [ad_service_1.AdService],
+        declarations: [app_component_1.AppComponent,
+            ad_banner_component_1.AdBannerComponent,
+            hero_job_ad_component_1.HeroJobAdComponent,
+            hero_profile_component_1.HeroProfileComponent,
+            ad_directive_1.AdDirective],
+        entryComponents: [hero_job_ad_component_1.HeroJobAdComponent, hero_profile_component_1.HeroProfileComponent],
+        bootstrap: [app_component_1.AppComponent]
+    }),
+    __metadata("design:paramtypes", [])
 ], AppModule);
 exports.AppModule = AppModule;
 //# sourceMappingURL=app.module.js.map
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { MissionControllerComponent } from './missioncontrol.component';
-import { AstronautComponent } from './astronaut.component';
+import { HeroJobAdComponent } from './hero-job-ad.component';
+import { AdBannerComponent } from './ad-banner.component';
+import { HeroProfileComponent } from './hero-profile.component';
+import { AdDirective } from './ad.directive';
+import { AdService } from './ad.service';
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  declarations: [
-    AppComponent,
-    MissionControllerComponent,
-    AstronautComponent
-  ],
-  bootstrap: [AppComponent],
-  providers: []
+  imports: [BrowserModule],
+  providers: [AdService],
+  declarations: [AppComponent,
+    AdBannerComponent,
+    HeroJobAdComponent,
+    HeroProfileComponent,
+    AdDirective],
+  entryComponents: [HeroJobAdComponent, HeroProfileComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() { }
+}
