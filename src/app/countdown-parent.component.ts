3c7dd50e04b2ddd84e02b70e55edeb0f5edebb2c
Chourasia, Ashok <ashok.chourasia@vce.com>
2017-07-01 18:15:07 +0530

    commit

--- src/app/countdown-parent.component.ts
+++ src/app/countdown-parent.component.ts
@@ -1,7 +1,21 @@
-import { Component }                from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { CountdownTimerComponent } from './countdown-timer.component'
 
 @Component({
-  selector: 'countdown-parent-lv',
+  selector: 'countdown-parent-vc',
   templateUrl: './countdown-parent.component.html'
 })
-export class CountdownLocalVarParentComponent { }
+export class CountdownLocalVarParentComponent implements AfterViewInit {
+  @ViewChild(CountdownTimerComponent)
+  private couterTimer: CountdownTimerComponent;
+  seconds() { return 0; };
+  ngAfterViewInit() {
+    setTimeout(() => this.seconds = () => this.couterTimer.seconds, 0);
+  }
+  start(): void {
+    this.couterTimer.start();
+  }
+  stop(): void {
+    this.couterTimer.stop();
+  }
+}
