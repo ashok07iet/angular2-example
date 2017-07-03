17d0079d63c1d21d1524442e8fc9af3449a9ad3c
Chourasia, Ashok <ashok.chourasia@vce.com>
2017-07-03 20:22:39 +0530

    commit

--- src/app/hero-birthday1.component.ts
+++ src/app/hero-birthday1.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'hero-birthday',
-    template: `<p>hero birthday is {{birthday|date}}</p>`
+    template: `<p>hero birthday is {{birthday|date:"MM/dd/yy"}}</p>`
 })
 export class HeroBirthdayComponent {
     birthday = new Date(1988, 3, 15);
