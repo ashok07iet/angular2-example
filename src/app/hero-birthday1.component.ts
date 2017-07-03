621e16e730944e959f54d7638a249a580e3ebca5
Chourasia, Ashok <ashok.chourasia@vce.com>
2017-07-03 20:43:14 +0530

    commit

--- src/app/hero-birthday1.component.ts
+++ src/app/hero-birthday1.component.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'hero-birthday',
-    template: `<p>hero birthday is {{birthday|date:"MM/dd/yy"}}</p>`
+    template: `<p>hero birthday is {{birthday|date:format}}</p>
+               <button (click)=toggleFormat()>toggle format</button>
+    `
 })
 export class HeroBirthdayComponent {
     birthday = new Date(1988, 3, 15);
+    toggle=true;
+    get format(){return this.toggle ? 'shortDate' : 'fullDate';}
+    toggleFormat(){
+        this.toggle=!this.toggle;
+    }
 }
