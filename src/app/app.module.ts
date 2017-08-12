0ebe3227967932be765209528e2796975ef5389a
choura <ashok.chourasia@vce.com>
2017-08-12 18:31:35 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { AuthInterceptor } from './auth.interceptor';
+import { TimimingInterceptor } from './timiming.interceptor';
 import { AuthService } from './auth.service';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 @NgModule({
@@ -17,7 +17,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
     AuthService,
     {
       provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
+      useClass: TimimingInterceptor,
       multi: true
     }
   ]
