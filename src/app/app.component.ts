634aef7c2de3c1f985112c49197dbb4700c38a1a
choura <ashok.chourasia@vce.com>
2017-08-13 13:33:51 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,32 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'tour of customer';
   firstCustomer = 'john';
+  customers = [{
+    'customerId': 1.0,
+    'FirstName': 'Marcus',
+    'LastName': 'HighTower',
+    'Email': 'Marcus.HighTower@acmecorp.com',
+    'Address': '1234 Anywhere St.',
+    'City': 'Phoenix',
+    'State': {
+      'Name': ' Arizona',
+      'Abbreviation': 'AZ'
+    },
+    'Zip': 85229.0,
+    'Gender': 'Male'
+  },
+  {
+    'customerId': 2.0,
+    'FirstName': 'Jesse',
+    'LastName': 'Smith',
+    'Email': 'Jesse.Smith@gmail.com',
+    'Address': '435 Main St.',
+    'City': 'Encinitas',
+    'State': {
+      'Name': 'California',
+      'Abbreviation': 'CA'
+    },
+    'Zip': 85230.0,
+    'Gender': 'Female'
+  }];
 }
