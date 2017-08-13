bcadb1682bf3c3d74c9e905dace55b486af4e9c9
choura <ashok.chourasia@vce.com>
2017-08-13 13:20:27 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HeroParent } from './hero-parent.component';
-import { HeroChild } from './hero-child.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent,
-    HeroParent,
-    HeroChild
+    AppComponent
   ],
   bootstrap: [AppComponent],
   providers: []
