e8c98269f046f18ad9e9273f8d7c02c047810f10
choura <ashok.chourasia@vce.com>
2017-08-17 21:48:40 +0530

    COMMIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,35 +5,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tour of customer';
-  firstCustomer = 'john';
-  isUnchaged = false;
-  inputCust = 'ashok';
-  customers = [{
-    'customerId': 1.0,
-    'FirstName': 'Marcus',
-    'LastName': 'HighTower',
-    'Email': 'Marcus.HighTower@acmecorp.com',
-    'Address': '1234 Anywhere St.',
-    'City': 'Phoenix',
-    'State': {
-      'Name': ' Arizona',
-      'Abbreviation': 'AZ'
-    },
-    'Zip': 85229.0,
-    'Gender': 'Male'
-  },
-  {
-    'customerId': 2.0,
-    'FirstName': 'Jesse',
-    'LastName': 'Smith',
-    'Email': 'Jesse.Smith@gmail.com',
-    'Address': '435 Main St.',
-    'City': 'Encinitas',
-    'State': {
-      'Name': 'California',
-      'Abbreviation': 'CA'
-    },
-    'Zip': 85230.0,
-    'Gender': 'Female'
-  }];
+  classes = ['alert-danger', 'text-center'];
+  actionName='click';
 }
