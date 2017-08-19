7d82b9429d22a7edeead5a798875dc58610dd527
choura <ashok.chourasia@vce.com>
2017-08-19 12:03:53 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,26 +4,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'tour of customer';
-  currentClasses = {};
-  currentStyles= {};
-  canSave = true;
-  isUnchanged = true;
-  isSpecial = true;
-  setCurrentClasses() {
-    // CSS classes: added/removed per current state of component properties
-    this.currentClasses = {
-      'saveable': this.canSave,
-      'modified': !this.isUnchanged,
-      'special': this.isSpecial
-    };
-  }
-  setCurrentStyles() {
-    // CSS styles: set per current state of component properties
-    this.currentStyles = {
-      'font-style': this.canSave ? 'italic' : 'normal',
-      'font-weight': !this.isUnchanged ? 'bold' : 'normal',
-      'font-size': this.isSpecial ? '24px' : '12px'
-    };
+  title = 'tour of Hero';
+  currentHero: {};
+  nullHero: {};
+  isActive = false;
+  constructor() {
+    this.currentHero = { name: 'Jhon' };
+    this.nullHero = { name: 'Rodrik' };
   }
 }
