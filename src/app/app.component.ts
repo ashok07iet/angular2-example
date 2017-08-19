6e379a50b54af09a8c3fdad6303acd3c66834158
choura <ashok.chourasia@vce.com>
2017-08-19 13:30:01 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,31 @@
-import { Component, AfterViewInit, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   title = 'tour of Hero';
-  heroes: any;
-  isActive = false;
-  heroesWithTrackByCountReset = 0;
-  heroIdIncrement = 1;
-  heroesNoTrackByCount = 0;
-  heroesWithTrackByCount = 0;
-  @ViewChildren('noTrackBy') heroesNoTrackBy: QueryList<ElementRef>;
-  @ViewChildren('withTrackBy') heroesWithTrackBy: QueryList<ElementRef>;
-  constructor() {
-    this.heroes = [{ id: 100, name: 'Jhon' }, { id: 101, name: 'Rodrik' }];
-  }
-  ngAfterViewInit() {
-    // Detect effects of NgForTrackBy
-    trackChanges(this.heroesNoTrackBy,   () => this.heroesNoTrackByCount++);
-    trackChanges(this.heroesWithTrackBy, () => this.heroesWithTrackByCount++);
-  }
-  resetHeroes() {
-    this.heroes = [{ id: 100, name: 'Jhon' }, { id: 101, name: 'Rodrik' }];
-    this.heroesWithTrackByCountReset = 0;
-  }
-  changeIds() {
-    this.resetHeroes();
-    this.heroes.forEach(h => h.id += 10 * this.heroIdIncrement++);
-    this.heroesWithTrackByCountReset = -1;
-  }
-  clearTrackByCounts() {
-    const trackByCountReset = this.heroesWithTrackByCountReset;
-    this.resetHeroes();
-    this.heroesNoTrackByCount = -1;
-    this.heroesWithTrackByCount = trackByCountReset;
-    this.heroIdIncrement = 1;
-  }
-  trackByHeroes(index: number, hero: any): number { return hero.id; }
+  currentHero =  {
+    name: 'Jhon',
+    id: 100,
+    emotion: 'happy'
+  };
 
-  trackById(index: number, item: any): number { return item['id']; }
+  heroes= [
+    {
+      name: 'Jhon',
+      id: 100,
+      emotion: 'happy'
+    },
+    {
+      name: 'jordan',
+      id: 101,
+      emotion: 'sad'
+    },
+    {
+      name: 'rodrik',
+      id: 102,
+      emotion: 'confused'
+    }
+  ];
 }
-  // helper to track changes to viewChildren
-  function trackChanges(views: QueryList<ElementRef>, changed: () => void) {
-    let oldRefs = views.toArray();
-    views.changes.subscribe((changes: QueryList<ElementRef>) => {
-      const changedRefs = changes.toArray();
-      // Check if every changed Element is the same as old and in the same position
-      const isSame = oldRefs.every((v, i) => v.nativeElement === changedRefs[i].nativeElement);
-      if (!isSame) {
-        oldRefs = changedRefs;
-        // wait a tick because called after views are constructed
-        setTimeout(changed, 0);
-      }
-    });
-  }
