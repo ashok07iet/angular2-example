818b13229fe91d4a7c3e1f5b9eabd9355a33b7b8
choura <ashok.chourasia@vce.com>
2017-08-21 23:38:06 +0530

    commi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AdService } from './ad.service';
+import { AdItem } from './ad-item';
+
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html'
+  template: `
+    <div>
+      <add-banner [ads]="ads"></add-banner>
+    </div>
+  `
 })
-export class AppComponent {
-  title = 'Angular 2 example';
+export class AppComponent implements OnInit {
+  ads: AdItem[];
 
+  constructor(private adService: AdService) { }
+
+  ngOnInit() {
+    this.ads = this.adService.getAds();
+  }
 }
