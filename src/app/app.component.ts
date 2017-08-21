ccd298226a5333fbbd5dee9196819918888dc248
choura <ashok.chourasia@vce.com>
2017-08-21 23:23:40 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styles: ['h1 { font-weight: bold; }']
 })
 export class AppComponent {
   title = 'Angular 2 example';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { MissionControllerComponent } from './missioncontrol.component';
-import { AstronautComponent } from './astronaut.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent,
-    MissionControllerComponent,
-    AstronautComponent
+    AppComponent
   ],
   bootstrap: [AppComponent],
   providers: []
